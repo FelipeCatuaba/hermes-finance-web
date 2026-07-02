9b2d90fd11d01727ced5770e5c479c7ab439c240
Felipe Bianchi Catuaba <felipe.catuaba@gmail.com>
2026-07-02 09:44:49 -0300

    feat(web): add expense edit delete actions

--- src/app/core/facades/expenses.facade.ts
+++ src/app/core/facades/expenses.facade.ts
@@ -16,9 +16,33 @@ export class ExpensesFacade {
     );
   }
 
+  update(id: string, payload: ExpenseCreateRequest) {
+    return this.api.updateExpense(id, payload).pipe(
+      tap((updated) => this.createdExpensesSubject.next(
+        this.createdExpensesSubject.value.map((expense) => expense.id === id ? updated : expense)
+      ))
+    );
+  }
+
+  delete(id: string) {
+    return this.api.deleteExpense(id).pipe(
+      tap(() => this.createdExpensesSubject.next(
+        this.createdExpensesSubject.value.filter((expense) => expense.id !== id)
+      ))
+    );
+  }
+
   createInstallments(payload: ExpenseInstallmentCreateRequest) {
     return this.api.createExpenseInstallments(payload).pipe(
       tap((expenses) => this.createdExpensesSubject.next([...expenses, ...this.createdExpensesSubject.value]))
+    );
+  }
+
+  deleteInstallmentGroup(id: string) {
+    return this.api.deleteInstallmentGroup(id).pipe(
+      tap(() => this.createdExpensesSubject.next(
+        this.createdExpensesSubject.value.filter((expense) => expense.installmentGroupId !== id)
+      ))
     );
   }
 }
